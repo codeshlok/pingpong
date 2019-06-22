72f419fd8abd76b334184700e27355d50140c0b9
Shlok Patil <shlok@vishals-air.lan>
2019-06-22 11:39:45 -0400

    Added paddele movement

--- bouncing_balls.py
+++ bouncing_balls.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 screen = pygame.display.set_mode((900,700))
@@ -6,9 +7,18 @@ screen = pygame.display.set_mode((900,700))
 finished = False # 0 < 10 - > True/ 10<10 ->False
 x = 0
 y = 350
-x2 = 890
+x2 = 815
 y2 = 350
 #print pygame.K_SPACE
+playerImage = pygame.image.load("Player1.png")
+playerImage = pygame.transform.scale(playerImage, (105,160))
+playerImage = playerImage.convert_alpha()
+
+playerImage2 = pygame.image.load("Player1.png")
+playerImage2 = pygame.transform.scale(playerImage2, (105,160))
+playerImage2 = playerImage2.convert_alpha()
+
+frame = pygame.time.Clock()
 
 while finished == False:
     for event in pygame.event.get():
@@ -25,13 +35,20 @@ while finished == False:
         y2 -= 5
     if pressedKeys[pygame.K_s] == 1:
         y2 += 5
-    if pressedKeys[pygame.K_SPACE] == 1:
-        x += 10
-    paddle = pygame.Rect(x, y, 10, 80)
-    paddle2 = pygame.Rect(x2, y2, 10, 80)
+    if pressedKeys[pygame.K_RIGHT] == 1:
+        x += 5
+    if pressedKeys[pygame.K_LEFT] == 1:
+        x -= 5
+    if pressedKeys[pygame.K_d] == 1:
+        x2 += 5
+    if pressedKeys[pygame.K_a] == 1:
+        x2 -= 5
+    dividor = pygame.Rect(450, 0, 20, 900)
     color = (255,255,255)
     black =(0,0,0)
     screen.fill(black)
-    pygame.draw.rect(screen, color, paddle)
-    pygame.draw.rect(screen, color, paddle2)
+    screen.blit(playerImage,(x,y))
+    screen.blit(playerImage2,(x2, y2))
+    pygame.draw.rect(screen, color, dividor)
     pygame.display.flip()
+    frame.tick(90) #FPS Limit
